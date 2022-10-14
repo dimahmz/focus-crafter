709bdfb0715afc3f79c64ea166ce0a751157dad1
ESHamza <essaadihamza4@outlook.com>
2022-10-14 19:21:46 +0100

    connect timer store to settings store

--- src/stores/timer.js
+++ src/stores/timer.js
@@ -1,127 +1,155 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
-// import { ref } from "vue";
 
-export const useCounterStore = defineStore("counter", {
-  state: () => {
-    return {
-      needToChange: false,
-      timerCountingIs: "promodro",
-      longBreakIntarval: 4,
-      //
-      confirmChange: false,
-      TimerIsCounting: false,
-      //
-      displayModel: false,
-      confirmChange: false,
-      //
-      todoPromodoros: 4,
-      startPromodoro: true,
-      promodoro: 90,
-      finishedPromodoros: 0,
-      //
-      startShortBreak: false,
-      shortBreak: 40,
-      //
-      startLongBreak: false,
-      longBreak: 100,
-      needLongBreak: false,
-      //
-    };
-  },
-  getters: {
-    getcounter: () => {},
-  },
-  actions: {
-    startPromodorotTimer() {
-      this.TimerIsCounting = this.startPromodoro = true;
-      this.timerCountingIs = "promodoro";
-      const interval = setInterval(() => {
-        if (this.needToChange) {
-          clearInterval(interval);
-          return;
+//setting store
+import { useSettingsStore } from "./settings";
+
+export const useCounterStore = defineStore(
+  "counter",
+
+  {
+    state: () => {
+      const store = useSettingsStore();
+      return {
+        pauseTimer: false,
+        needToChange: false,
+        startAlarm: false,
+        timerCountingIs: "promodro",
+        //
+        confirmChange: false,
+        TimerIsCounting: false,
+        //
+        displayModel: false,
+        confirmChange: false,
+        //
+        todoPromodoros: 4,
+        startPromodoro: true,
+        promodoro: store.promodoro_npt,
+        finishedPromodoros: 0,
+        //
+        startShortBreak: false,
+        shortBreak: store.shortBreak_npt,
+        //
+        startLongBreak: false,
+        longBreak: store.longBreak_npt,
+        needLongBreak: false,
+      };
+    },
+    getters: {
+      getcounter: () => {},
+      getTimerState() {
+        if (this.pauseTimer) return "=>";
+        return "||";
+      },
+    },
+    actions: {
+      startPromodorotTimer() {
+        const store = useSettingsStore();
+        this.TimerIsCounting = this.startPromodoro = true;
+        this.timerCountingIs = "promodoro";
+        const interval = setInterval(() => {
+          if (this.needToChange) {
+            clearInterval(interval);
+            return;
+          }
+          this.promodoro--;
+          if (this.promodoro == 2) this.startAlarm = true;
+          if (this.promodoro <= 0 || !this.startPromodoro) {
+            this.finishedPromodoros++;
+            !this.finishedPromodoros % store.rounds === 0
+              ? this.getAshortBreak(store.autoStartBreaks)
+              : this.getAlongBreak(store.autoStartBreaks);
+            clearInterval(interval);
+            this.promodoro = store.promodoro_npt;
+          }
+        }, 1000);
+      },
+      startShortBreakTimer() {
+        const store = useSettingsStore();
+        this.TimerIsCounting = true;
+        this.timerCountingIs = "shortBreak";
+        const interval = setInterval(() => {
+          if (this.needToChange) {
+            clearInterval(interval);
+            return;
+          }
+
+          this.shortBreak--;
+          if (this.shortBreak == 2) this.startAlarm = true;
+          if (this.shortBreak <= 0 || !this.startShortBreak) {
+            clearInterval(interval);
+            this.goToPromodoro(store.autoStartPromodoros);
+            this.shortBreak = store.shortBreak_npt;
+          }
+        }, 1000);
+      },
+      startLongBreakTimer() {
+        const store = useSettingsStore();
+        this.TimerIsCounting = true;
+        this.timerCountingIs = "longBreak";
+        const interval = setInterval(() => {
+          if (this.needToChange) {
+            clearInterval(interval);
+            return;
+          }
+          this.longBreak--;
+          if (this.longBreak == 2) this.startAlarm = true;
+          if (this.longBreak <= 0 || !this.startLongBreak) {
+            clearInterval(interval);
+            this.goToPromodoro(store.autoStartPromodoros);
+            this.longBreak = store.longBreak_npt;
+          }
+        }, 1000);
+      },
+
+      goToPromodoro(autochange) {
+        this.stopAll();
+        this.startPromodoro = true;
+        if (autochange) setTimeout(() => this.startPromodorotTimer(), 900);
+      },
+      getAshortBreak(autochange) {
+        this.stopAll();
+        this.startShortBreak = true;
+        if (autochange) setTimeout(() => this.startShortBreakTimer(), 900);
+      },
+      getAlongBreak(autochange) {
+        this.stopAll();
+        this.startLongBreak = true;
+        if (autochange) setTimeout(() => this.startLongBreakTimer(), 900);
+      },
+      changeToAnewPhase() {
+        this.displayModel = true;
+        this.needToChange = true;
+      },
+      changeAlarmSound(audioSrc) {
+        audio_elem.src = audioSrc;
+      },
+      needBreakConfirm(confirm) {
+        switch (this.timerCountingIs) {
+          case "promodoro":
+            if (confirm) this.promodoro = 0;
+            this.startPromodorotTimer();
+            break;
+          case "shortBreak":
+            if (confirm) this.shortBreak = 0;
+            this.startShortBreakTimer();
+            break;
+          case "longBreak":
+            if (confirm) this.longBreak = 0;
+            this.startLongBreakTimer();
+            break;
         }
-        if (this.promodoro === 0 || !this.startPromodoro) {
-          clearInterval(interval);
-          this.finishedPromodoros++;
-          if (this.finishedPromodoros % this.longBreakIntarval === 0) {
-            this.getAlongBreak();
-            this.longBreakIntarval = 4;
-          } else this.getAshortBreak();
-          this.promodoro = 9;
-        }
-        this.promodoro--;
-      }, 1000);
+        this.needToChange = this.displayModel = false;
+      },
+      stopAll() {
+        this.startPromodoro =
+          this.startShortBreak =
+          this.startLongBreak =
+          this.TimerIsCounting =
+            false;
+      },
     },
-    startShortBreakTimer() {
-      this.TimerIsCounting = true;
-      this.timerCountingIs = "shortBreak";
-      const interval = setInterval(() => {
-        if (this.needToChange) {
-          clearInterval(interval);
-          return;
-        }
-        if (this.shortBreak === 0 || !this.startShortBreak) {
-          this.goToPromodoro();
-          clearInterval(interval);
-          this.shortBreak = 4;
-        }
-        this.shortBreak--;
-      }, 1000);
-    },
-    startLongBreakTimer() {
-      this.TimerIsCounting = true;
-      this.timerCountingIs = "longBreak";
-      const interval = setInterval(() => {
-        if (this.needToChange) {
-          clearInterval(interval);
-          return;
-        }
-        if (this.longBreak === 0 || !this.startLongBreak) {
-          clearInterval(interval);
-          this.goToPromodoro();
-          this.longBreak = 10;
-        }
-        this.longBreak--;
-      }, 1000);
-    },
-    goToPromodoro() {
-      this.stopAll();
-      this.startPromodoro = true;
-    },
-    getAshortBreak() {
-      this.stopAll();
-      this.startShortBreak = true;
-    },
-    changeToAnewPhase() {
-      this.displayModel = true;
-      this.needToChange = true;
-    },
-    needBreakConfirm(confirm) {
-      if (this.timerCountingIs === "promodoro") {
-        if (confirm) this.promodoro = 0;
-        this.startPromodorotTimer();
-      } else if (this.timerCountingIs === "shortBreak") {
-        if (confirm) this.shortBreak = 0;
-        this.startShortBreakTimer();
-      } else {
-        if (confirm) this.longBreak = 0;
-        this.startLongBreakTimer();
-      }
-      this.needToChange = this.displayModel = false;
-    },
-    getAlongBreak() {
-      this.stopAll();
-      this.startLongBreak = true;
-    },
-    stopAll() {
-      this.startPromodoro =
-        this.startShortBreak =
-        this.startLongBreak =
-        this.TimerIsCounting =
-          false;
-    },
-  },
-});
+  }
+);
 
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useCounterStore, import.meta.hot));
