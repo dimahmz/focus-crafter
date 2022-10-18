6271d48e82f3603614b4602e1b8a720d8bcd030c
ESHamza <essaadihamza4@outlook.com>
2022-10-18 22:03:19 +0100

    subsribe to stores's state and stores any modification in localStorage

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,37 @@ WarningModel
 .hidden
   Settings
 </template>
-
 <script setup>
 import WarningModel from "./components/timer/warningModel.vue";
 import TimerBtns from "./components/timer/timerBtns.vue";
 import AppHeader from "./components/timer/appHeader.vue";
 import Footerbtns from "./components/timer/footerbtns.vue";
 import Settings from "./components/appSettings/settingModel.vue";
-</script>
+import { useTasksStore } from "./stores/tasks";
+import { useSettingsStore } from "./stores/settings";
+import { useCounterStore } from "./stores/timer";
 
+//stores
+
+const tasksStore = useTasksStore();
+const settingsStore = useSettingsStore();
+const timerStore = useCounterStore();
+
+//latest changes
+
+timerStore.$patch(JSON.parse(localStorage.getItem("counter")));
+tasksStore.$patch(JSON.parse(localStorage.getItem("tasks")));
+settingsStore.$patch(JSON.parse(localStorage.getItem("settings")));
+
+//subscribing to the latest changes
+
+settingsStore.$subscribe(setData);
+timerStore.$subscribe(setData);
+tasksStore.$subscribe(setData);
+function setData(mutation, state) {
+  localStorage.setItem(mutation.storeId, JSON.stringify(state));
+}
+</script>
 <style scoped>
 .header {
   @apply mt-4 mb-10;
