4f33563e796181cbb193c12dde8dcf29c8d8f63f
ESHamza <essaadihamza4@outlook.com>
2022-10-25 18:07:29 +0100

    range input component

---
+++ src/components/appRangeNpt.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.relative.pt-1
+  label(for='MusicVolume') {{nptLabel}}
+  input#MusicVolume(type='range' min=0 max=100 v-model="settingsStore[storeSet]" @click="$emit('change')")
+</template>
+
+<script setup>
+import { useSettingsStore } from "@/stores/settings";
+
+const settingsStore = useSettingsStore();
+const props = defineProps({
+  nptValue: { required: true },
+  nptLabel: { type: String },
+  storeSet: { type: String, required: true },
+});
+</script>
+
+<style scoped>
+input {
+  @apply appearance-none w-full h-1 p-0 bg-white focus:outline-none focus:ring-0 focus:shadow-none;
+}
+</style>
