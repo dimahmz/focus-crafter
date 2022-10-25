efd3b7fb4b4d6304cd4aedacaca34d888ec4f532
ESHamza <essaadihamza4@outlook.com>
2022-10-25 18:08:26 +0100

    selelct input component

---
+++ src/components/appSelectInput.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+select(v-model="settingsStore.selectedAlarm")
+  option(
+    v-for="option in settingsStore.alarmSound" 
+    :value="option.value"
+    @click="$emit('change')"
+  ) {{ option.text }}
+</template>
+
+<script setup>
+import { useSettingsStore } from "@/stores/settings";
+const settingsStore = useSettingsStore();
+</script>
+
+<style scoped>
+section {
+  @apply flex justify-between;
+}
+.npts-container {
+  @apply flex flex-col space-y-4;
+}
+</style>
