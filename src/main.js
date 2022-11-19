917d08eb3ec94fd9a3143611869e5f9a5a5eeda9
ESHamza <essaadihamza4@outlook.com>
2022-11-19 17:16:49 +0100

    ant-design integration

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import Antd from "ant-design-vue";
 import App from "./App.vue";
 
+import "ant-design-vue/dist/antd.css";
 import "./assets/main.css";
 import "./assets/style.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(Antd);
 
 app.mount("#app");
