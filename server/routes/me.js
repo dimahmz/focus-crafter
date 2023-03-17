a759df2fe5b7873a7469e8c62c4af9f556ff89c7
dimahmz <essaadihamza4@outlook.com>
2023-03-17 20:57:06 +0100

    user profile  api

--- server/routes/me.js
+++ server/routes/me.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const auth = require("../middleware/authenticated");
 const { User }=require("../models/user");
 
-router.post("/me", auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select("-password -tasks -settings");
+router.post("/", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password  -verifivation_token");
   res.send(user);
 });
+
+module.exports= router;
