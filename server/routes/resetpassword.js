f9519563409ff4c23f415ff571e8c406581c9921
dimahmz <essaadihamza4@outlook.com>
2023-03-17 20:56:20 +0100

    forget a password api

--- server/routes/resetpassword.js
+++ server/routes/resetpassword.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("../models/user");
-const {
-  resetPassword,
-  verifyPasswordResetToken,
-} = require("../models/resetPassword");
-const bcrypt = require("bcrypt");
-const logger = require("../middleware/logger");
+const { resetPassword, verifyPasswordResetToken } = require("../models/resetPassword.model.js");
+const userServices = require("../services/userServices");
+const Joi =require("joi");
 
 router.get("/", async (req, res) => {
   res.send("reset your password page");
@@ -14,55 +11,59 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
-
-  if (!user) return res.send("reset link has been sent to this email!");
+  if (!user)
+    return res.status(200).send("reset link has been sent to this email!");
 
   let resetToken = await resetPassword.findOne({ user_id: user.id });
-  if (resetToken) return res.send("reset link was already send!");
+  if (resetToken) return res.status(200).send("reset link has been sent to this email!");
 
-  resetToken = new resetPassword({ user_id: user.id });
-  await resetToken.save();
+  try{
+   const isLinkSent = await userServices.sendResetPasswordLink(user.email, user.id);
 
-  // @todo: send an email that contains a reset link  to reset password
-
-  res.status(200).send("reset link has been sent!");
+   if (isLinkSent) res.status(200).send("reset link has been sent to this email!");
+  } catch(e){
+    console.log(e.message);
+    return res.status(500).send("an internal server was occurred, please try again later!");
+   }
 });
 
 router.get("/:user_id/:reset_token", async (req, res) => {
-  const resetToken = await verifyPasswordResetToken(
-    req.params.user_id,
-    req.params.reset_token
-  );
 
-  if (!resetToken) return res.status(404).send("invalid token!");
-
+  //verification is made inside the resetpassword model
+  const isTokenExist = await verifyPasswordResetToken(req.params.user_id,req.params.reset_token);
+  if (!isTokenExist) return res.status(404).send("invalid token!");
   res.status(200).send("entre a new password");
+
 });
 
-router.post("/:user_id/:reset_token", async (req, res) => {
-  const resetToken = await verifyPasswordResetToken(
-    req.params.user_id,
-    req.params.reset_token
-  );
-  if (!resetToken) return req.status(404).send("invalid link!");
+router.put("/:user_id/:reset_token", async (req, res) => {
+  const isTokenExist = await verifyPasswordResetToken(req.params.user_id,req.params.reset_token);
+  if (!isTokenExist) return res.status(404).send("invalid link!");
 
-  const user = await User.findById(resetToken.user_id);
+  //whtat the user was not found
+  const user = await User.findById(req.params.user_id);
   if (!user) throw new error("user not found!");
+  const passwords={old_password: req.body.old_password, new_password: req.body.new_password};
+  const {error}= validatePasswords(passwords);
+  if(error) return res.status(400).send(error.message);
 
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(404).send("invalid password!");
-
-  try {
-    const slat = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(req.body.new_password, slat);
-    await user.save();
-  } catch (e) {
-    logger.error(e);
-    res.status(500).send("an error has been occured");
-  } finally {
-     await resetPassword.findOneAndDelete({ user_id: req.params.user_id, reset_token: req.params.reset_token });
+  try
+  {
+    await userServices.changePassword(user, req.body.old_password, req.body.new_password);
+    res.status(200).send("password updated successfully!");
   }
-  res.status(200).redirect("/auth");
+  catch(e){
+    if(e) return res.status(400).send(e.message);
+    return res.status(500).send("an error has been occured");
+  }
 });
+
+function validatePasswords(passwords){
+  const schema = Joi.object({
+    old_password: Joi.string().min(4).max(255).required(),
+    new_password: Joi.string().min(8).max(255).required().invalid(Joi.ref("old_password")),
+  });
+  return schema.validate(passwords);
+}
 
 module.exports = router;
