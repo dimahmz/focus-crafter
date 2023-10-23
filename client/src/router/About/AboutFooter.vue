0e9a52551b365476d4af10e33c473dab2eac2abc
dimahmz <essaadihamza4@outlook.com>
2023-10-23 03:57:07 +0100

    fix: failed to resolve import from public

--- client/src/router/About/AboutFooter.vue
+++ client/src/router/About/AboutFooter.vue
@@ -1,11 +1,10 @@
-<script setup></script>
 <template lang="pug">
 footer.text-xl.font-semibold.py-10
   nav.flex.space-x-4.px-8
     p.text-app-tertiary Developed by Hamza ,
     .flex.space-x-5
       a(target="_blank" href="https://twitter.com/_hashirra")
-        img.h-6.w-6(src="twitter-mark-black.png")
+        img.h-6.w-6(src="/twitter-mark-black.png")
       a.inline-block.h-10.w-10(target="blank" href="https://github.com/dimahmz/pro-focus") 
-        img.h-6.w-6(src="github-mark.png")
+        img.h-6.w-6(src="/github-mark.png")
 </template>
